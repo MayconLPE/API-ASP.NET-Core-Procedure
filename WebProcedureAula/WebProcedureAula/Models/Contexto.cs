c15fb4a350ac6da1bfadb94e1083eded4324af36
MayconL27 <mayconleandro2008@gmail.com>
2024-08-13 16:23:20 -0300

    Primeiro commit

---
+++ WebProcedureAula/WebProcedureAula/Models/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebProcedureAula.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
